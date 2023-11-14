0588ace6163fc1e4e05212b9aee8434fd13a0a1f
Vladislav-Vasilev96 <Vladdoo96@abv.bg>
2023-11-14 20:54:21 +0200

    Add ShoppingList directory

---
+++ ShopingList/script.js
@@ -0,0 +1,40 @@
+
+function addItem() {
+    let newItem = document.getElementById('itemInput').value;
+
+    if (newItem.trim() !== '') {
+        let li = document.createElement('li');
+        li.textContent = newItem;
+
+        let editBtn = document.createElement('button');
+        editBtn.textContent = 'Edit';
+        editBtn.addEventListener('click', function () {
+            editItem(li);
+        });
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', function () {
+            deleteItem(li);
+        });
+
+        li.appendChild(editBtn);
+        li.appendChild(deleteBtn);
+
+        document.getElementById('shoppingList').appendChild(li);
+
+        document.getElementById('itemInput').value = '';
+    }
+}
+
+function editItem(li) {
+    document.getElementById('itemInput').value = li.textContent  ;
+
+    li.remove();
+}
+
+function deleteItem(li) {
+    li.remove();
+}
+
+document.getElementById('addItemBtn').addEventListener('click', addItem);
